6d9f3f854c0d2d73a9b644d5aa66b11ae6177552
Jakub Vitásek <jakub.vitasek@educhem.cz>
2022-05-30 09:03:59 +0200

    Přidejte soubory projektu.

---
+++ SpellBook/HelperClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpellBook
+{
+    internal class HelperClass
+    {
+        public Random generator = new Random();
+        public string[] PoleZTextu(string adresaSouboru)
+        {
+            string obsahTxt = File.ReadAllText("plocha/spellform.txt");
+            string obsahTxt2 = File.ReadAllText("plocha/spelltypes.txt");
+            int delka1 = obsahTxt.Length;
+            int delka2 = obsahTxt2.Length;
+            string[] poleForma = obsahTxt.Split(";");
+            string[] poleTypy = obsahTxt.Split(";");
+
+        }
+        public string VygenerujemeSpellName(string[] poleForma, string[] poleTypy)
+        {
+            int indexForma = VygenerujCisloMax(poleForma.Length - 1);
+            int indexTypy = generator.Next(0, poleTypy.Length - 1);
+            string spojene = (indexForma + indexTypy).ToString();
+            return spojene;
+
+        }
+        public int VygenerujCisloMax(int max)
+        {
+            int cislo = generator.Next(0, max);
+            return cislo;
+        }
+
+
+
+    }
+}
---
+++ SpellBook/Program.cs
@@ -0,0 +1,46 @@
+﻿global using SpellBook;
+
+
+Console.WriteLine("Generátor náhodných spellů");
+
+List<Spell> seznamSpellu = new List<Spell>();
+HelperClass pomocneMetody = new HelperClass();  
+
+string[] poleForma = pomocneMetody.PoleZTextu("plocha/spellform.txt");
+string[] poleTypy = pomocneMetody.PoleZTextu("plocha/spelltypes.txt");
+
+while (true)
+{
+    Console.WriteLine("1. vygenerovat nový spell a přidat do Spellbooku");
+    Console.WriteLine("2. zobrazit Spellbook");
+    Console.WriteLine("3. smazat poslední záznam ve Spellbooku");
+    Console.WriteLine("4. smazat celý Spellbook");
+    Console.WriteLine("*. ukončit aplikaci");
+
+
+    string input = Console.ReadLine();
+
+    if (input == "1")
+    {
+        Spell newspell = new Spell();
+        newspell.Name = pomocneMetody.VygenerujemeSpellName(poleForma, poleTypy);
+        newspell.ManaCost = pomocneMetody.VygenerujCisloMax(500);
+        seznamSpellu.Add(newspell);
+    }
+    else if (input == "2")
+    {
+
+    }
+    else if (input == "3")
+    {
+
+    }
+    else if (input == "4")
+    {
+
+    }
+    else if (input == "*")
+    {
+
+    }
+}
